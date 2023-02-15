006f70c627c07c61c39aaf30fc6d930452feab80
Cass <huladollx@hotmail.com>
2023-02-15 14:46:56 +1000

    testchanges

--- utils/article.js
+++
@@ -1,9 +0,0 @@
-export const getLocalArticlesDetails = articles => {
-	return articles.map(article => {
-		return {
-			title: article.title,
-			slug: article.slug,
-			createdAt: new Date(article.createdAt)
-		};
-	});
-};
--- utils/cacheHelper.js
+++
@@ -1,6 +0,0 @@
-const NodeCache = require('node-cache');
-
-export const universityCache = new NodeCache({
-	stdTTL: 86400,
-	checkperiod: 14400
-});
